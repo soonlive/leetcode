638976cbf5423618e9dfe8188a6ef80a1703cf6c
soonlive <soon@live.cn>
2017-03-05 22:53:22 +0800

    Iperformance optimization of 122. Best Time to Buy and Sell Stock I

--- 122. Best Time to Buy and Sell Stock II.js
+++ 122. Best Time to Buy and Sell Stock II.js
@@ -11,7 +11,9 @@ var maxProfit = function (prices) {
   let profit = 0;
 
   for (let i = 1; i < prices.length; i++) {
-    profit = profit + Math.max(prices[i] - prices[i - 1], 0);
+    if(prices[i]> prices[i-1] ){
+      profit = profit + Math.max(prices[i] - prices[i - 1], 0);
+    }
   }
 
   return profit;
