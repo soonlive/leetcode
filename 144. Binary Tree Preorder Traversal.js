3d42f6541f8944d772715296f03bd35a0592f6fc
Sean Hui <soon@live.cn>
2022-04-08 11:06:06 +0800

    give 144. Binary Tree Preorder Traversal

---
+++ 144. Binary Tree Preorder Traversal.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Xin on 2022/4/8.
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+let res = []
+var preorderTraversal = function(root) {
+  let res = []
+
+  let traverse = function(p){
+    if(null === p) return [];
+
+    res.push(p.val);
+    traverse(p.left);
+    traverse(p.right);
+
+  };
+
+  traverse(root);
+
+  return res;
+};
