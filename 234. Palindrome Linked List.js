70e18a503f9edcc3d77a90dcea0b0d6cf085fef6
Sean Hui <soon@live.cn>
2022-05-06 11:21:39 +0800

    234. Palindrome Linked List

--- 234. Palindrome Linked List.js
+++ 234. Palindrome Linked List.js
@@ -1,8 +1,4 @@
 /**
- * Created by Xin on 2022/5/6.
- */
-
-/**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
@@ -14,26 +10,46 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
+  if(!head && !head.next) return false;
+
   let fast = head;
   let slow = head;
-  let v = [head.val];
+  let mid = null;
   while(fast && fast.next){
     fast = fast.next.next;
-    slow = slow.next;
-    v.push(slow.val);
-  }
-
-  if(!fast){
-    v.pop();
-  }
-
-  while(slow){
-    let top = v.pop();
-    if(slow.val !== top){
-      return false;
+    if(null == fast){
+      mid = slow;
     }
     slow = slow.next;
   }
 
+  if(fast){
+    mid = slow;
+  }
+
+  mid = reverse(mid);
+
+  while(mid && head){
+    if(mid.val !== head.val){
+      return false;
+    }
+    mid = mid.next;
+    head = head.next;
+  }
+
   return true;
 };
+
+let reverse = function(node){
+  if(!node || !node.next) return node;
+
+  let cur = node;
+  let pre = null;
+  while(cur){
+    let temp = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = temp;
+  }
+  return pre;
+}
