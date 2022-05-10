31c3d118f1b19d413a905905a302e7b9ff8993be
Sean Hui <soon@live.cn>
2022-05-10 10:51:37 +0800

    392. Is Subsequence

--- 392. Is Subsequence.js
+++ 392. Is Subsequence.js
@@ -1,11 +1,26 @@
 /**
- * Created by Xin on 02/05/2017.
- */
-/**
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
+  if(!s) return true;
+  if(!t) return false;
 
+
+  let dp = [];
+  for(let i =0;i<s.length;++i){
+    dp[i] = [s.at(i) === t.at(0)];
+  }
+
+  for(let j =0;j<t.length;++j){
+    dp[0][j] = s.at(0) === t.at(j) || dp[0][j-1];
+  }
+
+  for(let i=1;i<s.length;++i){
+    for(let j=1;j<t.length;++j){
+      dp[i][j] = !!(dp[i - 1][j] && dp[i - 1][j - 1] && (s.at(i) === t.at(j) || dp[i][j - 1]));
+    }
+  }
+  return dp[s.length-1][t.length-1] === true;
 };
